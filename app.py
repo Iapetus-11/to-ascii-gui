8e43ecc9f763849154e62a3949579226a1a99d0c
Iapetus-11 <olimdahacker@gmail.com>
2020-08-31 11:16:25 -0400

    remove fixed size attempt for now

--- app.py
+++ app.py
@@ -142,7 +142,6 @@ class App(QWidget):
             self.layout.addLayout(sub_layout)
 
         self.setLayout(self.layout)
-        self.setFixedSize(self.width(), self.height())
         self.show()
 
     def connect_source_button(self, _type):
