cb668120490733f7f311b405a507e70c1b8eb0b4
Iapetus-11 <olimdahacker@gmail.com>
2020-08-30 21:40:41 -0400

    attempt to fix formatting for scale

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ class App(QWidget):
 
     def connect_scale_slider(self, value):
         try:
-            self.scale_label.setText(f'Scale: {(value + 1) / 100}x')
+            self.scale_label.setText('Scale: {:1.2f}x'.format((value + 1) / 100))
         except AttributeError:
             pass
 
