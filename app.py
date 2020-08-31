01a0ac1c20f5f0151770f237c80fe160a9d0e075
Iapetus-11 <olimdahacker@gmail.com>
2020-08-30 21:54:01 -0400

    remove extra )

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ class App(QWidget):
 
     def connect_scale_slider(self, value):
         try:
-            self.scale = (value + 1) / 100)
+            self.scale = (value + 1) / 100
             self.scale_label.setText('Scale: {:1.2f}x'.format(self.scale)
         except AttributeError:
             pass
